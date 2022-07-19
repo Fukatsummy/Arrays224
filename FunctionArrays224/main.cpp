96b86f01c6d48cdfefa3dd9de06a326f4f672011
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-07-19 21:28:48 +0300

    Template

--- FunctionArrays224/main.cpp
+++ FunctionArrays224/main.cpp
@@ -6,22 +6,23 @@ using namespace std;
 const int ROWS = 3;
 const int COLS = 4;
 
+
 void FillRand(int arr[], const int n);
 void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS],const int ROWS, const int COLS);
+//void FillRand(int arr[ROWS][COLS],const int ROWS, const int COLS);
 
-void Print(int arr[], const int n);
-void Print(double arr[], const int n);
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+template <typename T>void Print(T arr[], const int n);
+template <typename T>void Print( T arr[], const int n);
+//void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
-void Sort(int arr[], const int n);
-void Sort(double arr[], const int n);
+template <typename T>void Sort(T arr[], const int n);
+template <typename T>void Sort(T arr[], const int n);
 
-int Sum(int arr[], const int n);
-double Sum(double arr[], const int n);
+template <typename T>T Sum(T arr[], const int n);
+template <typename T>T Sum(T arr[], const int n);
 
-double Avg(int arr[], const int n);
-double Avg(double arr[], const int n);
+template <typename T>double Avg(T arr[], const int n);
+template <typename T>double Avg(T arr[], const int n);
 
 
 void main()
@@ -46,10 +47,11 @@ void main()
 	cout << "Средне-арифметическое элементов массива: " << Avg(brr, SIZE) << endl;
 
 	
-	int i_arr_2[ROWS][COLS];
+	/*int i_arr_2[ROWS][COLS];
 	FillRand(i_arr_2, ROWS, COLS);
-	Print(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);*/
 }
+
 
 void FillRand(int arr[], const int n)
 {
@@ -66,7 +68,8 @@ void FillRand(double arr[], const int n)
 		arr[i] /= 100;
 	}
 }
-void Print(int arr[], const int n)
+
+template <typename T>void Print(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -74,32 +77,15 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
+//template <typename T>void Print(T arr[], const int n){
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << arr[i] << tab;
+//	}
+//	cout << endl;
+//}
 
-void Print(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-
-void Sort(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-void Sort(double arr[], const int n)
+template <typename T>void Sort(T arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -107,17 +93,32 @@ void Sort(double arr[], const int n)
 		{
 			if (arr[j] < arr[i])
 			{
-				double buffer = arr[i];
+				T buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
 		}
 	}
 }
+//template <typename T>void Sort(T arr[], const int n)
+//{
+//	for (int i = 0; i < n; i++)
+//	{
+//		for (int j = i + 1; j < n; j++)
+//		{
+//			if (arr[j] < arr[i])
+//			{
+//				T buffer = arr[i];
+//				arr[i] = arr[j];
+//				arr[j] = buffer;
+//			}
+//		}
+//	}
+//}
 
-int Sum(int arr[], const int n)
+template <typename T>T Sum(T arr[], const int n)
 {
-	int sum = 0;
+	T sum = 0;
 	for (int i = 0; i < n; i++)
 	{
 		sum += arr[i];
@@ -125,22 +126,22 @@ int Sum(int arr[], const int n)
 	return sum;
 
 }
+ //template <typename T>T Sum(T arr[], const int n)
+//{
+//	T sum = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum += arr[i];
+//	}
+//	return sum;
+//
+//}
 
-double Sum(double arr[], const int n)
-{
-	double sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-
-}
-double Avg(int arr[], const int n)
+ template <typename T>double Avg(T arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
-double Avg(double arr[], const int n)
-{
+//template <typename T>double Avg(T arr[], const int n)
+/*
 	return (double) Sum(arr, n) / n;
-}
+}{*/
